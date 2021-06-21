16aead7644bba4680f67c4f41cd05f1b1197b548
shibing624 <shibing624@126.com>
2021-06-21 21:35:29 +0800

    update to 0.2.2

--- addressparser/__init__.py
+++ addressparser/__init__.py
@@ -13,7 +13,7 @@ import pandas as pd
 from .structures import AddrMap, Pca
 from .structures import P, C, A
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 pwd_path = os.path.abspath(os.path.dirname(__file__))
 # 区划地址文件
